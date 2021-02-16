eb0121fdcfce57d2bf542f22f0a20a62a9bbe402
Maurice Boothe Jr <boothemjr@users.noreply.github.com>
2021-02-15 23:21:05 -0500

    re-initial commit

---
+++ ZombieRun/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+
+    private static int _currentLevel;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    public static void AdvanceCurrentLevel()
+    {
+        SceneManager.LoadScene(++_currentLevel);
+    }
+}
---
+++ ZombieRun/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float forceAmount = 5;
+    private Rigidbody _rb;
+    
+    private void Awake()
+    {
+        transform.position = GameObject.Find("StartPos").transform.position;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // assign rigidbody of this object when created
+        _rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // WASD CONTROLS
+        if (Input.GetKey(KeyCode.W))
+        {
+            _rb.AddForce(Vector3.forward * forceAmount);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _rb.AddForce(Vector3.left * forceAmount);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _rb.AddForce(Vector3.back * forceAmount);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _rb.AddForce(Vector3.right * forceAmount);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "EndPos")
+        {
+            GameManager.AdvanceCurrentLevel();
+        }
+    }
+}
